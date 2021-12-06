aa98e267a80ff5cffa8090c7649b8e21bf45b77b
Fidel Coria <fidel.coria@aa.com>
2021-12-06 09:26:58 -0600

    add hero text contrast

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
+      <div className={clsx("container", styles.heroContrast)}>
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
       </div>
